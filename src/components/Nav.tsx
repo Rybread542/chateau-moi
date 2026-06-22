4d9328feb255605bce4da399d06640de69e96a1c
Ryan Yohe <rybread054@gmail.com>
2026-06-21 19:04:35 -0600

    homepage + navbutton

---
+++ src/components/Nav.tsx
@@ -0,0 +1,36 @@
+"use client"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Nav() {
+    const LINKS = [
+        {
+            href: "/",
+            label: "Home"
+        },
+        {
+            href: "/blog",
+            label: "Blog"
+        },
+        {
+            href: "/tools",
+            label: "Tools"
+        },
+    ]
+
+    const path = usePathname()
+
+    if (path !== '/'){
+        return (
+
+            <nav className="">
+                {LINKS.map(link => (
+                    <Link key={link.href} href={link.href}>
+                        {link.label}
+                    </Link>
+                ))}
+
+            </nav>
+        )
+    }
+}
---
+++ src/components/navbutton.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+
+
+interface ButtonProps {
+    text: string;
+    navTo: string;
+}
+
+export default function NavButton({text, navTo}: ButtonProps) {
+
+    return(
+        <Button variant={'outline'} size={'lg'}>
+            <Link href={navTo}>{text}</Link>
+        </Button>
+    )
+}
